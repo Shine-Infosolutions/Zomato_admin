8e7ddf66a7477010965fd10c7b2c446c70de25aa
priyansh911911 <priyanshgkp9999@gmail.com>
2025-10-17 01:54:56 +0530

    ref

--- ref/src/pages/OrderConfirmationPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import OrderConfirmation from "../components/OrderConfirmation";
-
-const OrderConfirmationPage = () => {
-  return <OrderConfirmation />;
-};
-
-export default OrderConfirmationPage;
